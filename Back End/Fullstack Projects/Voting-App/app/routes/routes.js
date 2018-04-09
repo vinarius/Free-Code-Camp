0457f24493fa1eb01d1da41dbfa82eff650083af
Mark Kraus <mark.kraus001@gmail.com>
2018-04-09 15:54:39 -0400

    removed success.pug. added a pug template for 404 page

--- Back End/Fullstack Projects/Voting-App/app/routes/routes.js
+++ Back End/Fullstack Projects/Voting-App/app/routes/routes.js
@@ -4,12 +4,12 @@ module.exports = (app) => {
 
     app.route('/')
         .get((req, res) => {
-            res.render('home', {title: "Voting App | Home"});
+            res.render('home', {title: "Vin Votes | Home"});
         });
 
     app.use((req, res, next)=>{
         res.status(404);
-        res.type('txt').send("404, route not found. :(");
+        res.render('404', {title: "Vin Votes | 404"});
     });
 
 }
