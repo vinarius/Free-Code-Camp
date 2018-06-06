02d2a2acbb3751f1edb1549bde6212c57fceb00a
Mark Kraus <mark.kraus001@gmail.com>
2018-06-06 16:34:14 -0400

    finished product landing page

--- Current/1-Responsive-Web-Design/Product-Landing-Page/main.js
+++ Current/1-Responsive-Web-Design/Product-Landing-Page/main.js
@@ -10,12 +10,29 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("main").setAttribute('style', `min-height: ${customFooterPosition}px`);
     }
 
+    function subtitleContainerAutoHeight(){
+        let subtitleContainer = document.getElementsByClassName('subtitle-container');
+        let titleDiv = document.getElementsByClassName('instrument-subcontainer-title');
+        for(let i=0; i<subtitleContainer.length; i++){
+            let titleHeight = window.getComputedStyle(titleDiv[i], null);
+            let mutateTitle = titleHeight.height;
+            mutateTitle = mutateTitle.substring(0, (mutateTitle.length - 2));
+            mutateTitle = Number(mutateTitle);
+            let overallContainerHeight = document.getElementsByClassName('instrument-subcontainer')[0].offsetHeight;
+            let result = overallContainerHeight - mutateTitle;
+            document.getElementById(`subtitle-container${i}`).setAttribute('style', `height: 0px`);
+            document.getElementById(`subtitle-container${i}`).setAttribute('style', `height: ${result}px`);
+        }
+    }
+
     //on page load
     footerAutoHeight();
+    subtitleContainerAutoHeight();
 
     //on window resize
     window.addEventListener('resize', ()=>{
         footerAutoHeight();
+        subtitleContainerAutoHeight();
     });
 
 }); //end of document ready
