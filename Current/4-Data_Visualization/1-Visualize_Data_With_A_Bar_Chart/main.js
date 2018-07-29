a21f1d99f78e8a5f8a0593f710df8f60a49c56c4
Mark Kraus <mark.kraus001@gmail.com>
2018-07-28 22:01:39 -0400

    setting up initial front end without canvas.

--- Current/4-Data_Visualization/1-Visualize_Data_With_A_Bar_Chart/main.js
+++ Current/4-Data_Visualization/1-Visualize_Data_With_A_Bar_Chart/main.js
@@ -1,5 +1,30 @@
 window.addEventListener("DOMContentLoaded", function(){
     
-    
+    const h = 550;
+    const w = 900;
+    const padding = 20;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", true);
+    xhr.send();
+    xhr.onload=function(){
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.data);
+    }
+
+    // const xScale = d3.scaleLinear()
+    //                 .domain([0])
+    //                 .range([]);
+
+    // const yScale = d3.scaleLinear()
+    //                 .domain([])
+    //                 .range([]);
+
+    const svg = d3.select("main")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .attr("class", "customMt-5 customMb-5")
+        .attr("id", "bar-chart-svg-js");
 
 });//end of doc ready
