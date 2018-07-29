9b9a452c57a057dfa8891ade3201eaaae9feffd7
Mark Kraus <mark.kraus001@gmail.com>
2018-07-28 23:31:49 -0400

    added axis to chart

--- Current/4-Data_Visualization/1-Visualize_Data_With_A_Bar_Chart/main.js
+++ Current/4-Data_Visualization/1-Visualize_Data_With_A_Bar_Chart/main.js
@@ -1,30 +1,51 @@
-window.addEventListener("DOMContentLoaded", function(){
-    
+window.addEventListener("DOMContentLoaded", function () {
+
     const h = 550;
     const w = 900;
-    const padding = 20;
+    const padding = 60;
 
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", true);
     xhr.send();
-    xhr.onload=function(){
+    xhr.onload = function () {
         var json = JSON.parse(xhr.responseText);
         console.log(json.data);
+        createChart(json.data);
     }
 
-    // const xScale = d3.scaleLinear()
-    //                 .domain([0])
-    //                 .range([]);
+    function createChart(data) {
 
-    // const yScale = d3.scaleLinear()
-    //                 .domain([])
-    //                 .range([]);
-
-    const svg = d3.select("main")
+        const svg = d3.select("main")
         .append("svg")
         .attr("width", w)
         .attr("height", h)
         .attr("class", "customMt-5 customMb-5")
         .attr("id", "bar-chart-svg-js");
 
-});//end of doc ready
+        const xScale = d3.scaleLinear()
+            .domain([0, d3.max(data, (d)=> {
+                let dataString = d[0].split('-');
+                dataString = dataString[0];
+                return Number(dataString);
+            })])
+            .range([padding, w - padding]);
+
+        const yScale = d3.scaleLinear()
+                        .domain([0, d3.max(data, (d)=> d[1])])
+                        .range([h - padding, padding]);
+
+        const xAxis = d3.axisBottom(xScale);
+        const yAxis = d3.axisLeft(yScale);
+
+        svg.append('g')
+            .attr('transform', 'translate(0, ' + (h - padding) + ')')
+            .call(xAxis);
+
+        svg.append("g")
+            .attr('transform', 'translate(' + padding + ', 0)')
+            .call(yAxis);
+
+    }
+
+
+}); //end of doc ready
