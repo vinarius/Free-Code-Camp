b6eef71c85f3526a8a3c97b48804eff5e7e3bec5
Mark <vindevccm@gmail.com>
2018-12-30 19:03:56 -0500

    basic layout setup

--- Current/Extra/Take Home Projects/light-bright/src/app/app.module.ts
+++ Current/Extra/Take Home Projects/light-bright/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LightBrightComponent } from './light-bright/light-bright.component';
+import { LightComponent } from './light-bright/light/light.component';
+import { UserStoriesComponent } from './user-stories/user-stories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LightBrightComponent,
+    LightComponent,
+    UserStoriesComponent
   ],
   imports: [
     BrowserModule
---
+++ Current/Extra/Take Home Projects/light-bright/src/app/user-stories/user-stories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-stories',
+  templateUrl: './user-stories.component.html',
+  styleUrls: ['./user-stories.component.css']
+})
+export class UserStoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
