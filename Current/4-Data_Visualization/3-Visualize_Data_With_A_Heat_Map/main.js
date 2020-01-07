d05415c85def86aec31ea1e79f78dfffcec37497
Mark Kraus <markraus@deloitte.com>
2020-01-06 20:51:59 -0500

    minor additions to heatmap project

--- Current/4-Data_Visualization/3-Visualize_Data_With_A_Heat_Map/main.js
+++ Current/4-Data_Visualization/3-Visualize_Data_With_A_Heat_Map/main.js
@@ -1,13 +1,13 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-    
+window.addEventListener('DOMContentLoaded', () => {
+
     let height = 0;
     let width = 0;
 
     const margin = {
-        top: 0, // 50
-        right: 0, // 20
-        bottom: 0, // 100
-        left: 0 // 80
+        top: 50,
+        right: 20,
+        bottom: 100,
+        left: 80
     };
 
     const months = [
@@ -27,8 +27,40 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
     let baseTemperature;
 
-    function convertNumberToMonth (int) {
-        switch(int) {
+    const testText = document.getElementById('test');
+
+    const padding = 40;
+    const heatMap = document.getElementById('heatMap');
+    const jsonURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
+    // const colorScheme = d3.scaleOrdinal(d3.schemeBlues[11]);
+    const color = d3.scaleOrdinal()
+        .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        .range([
+            '#343797', // 0
+            '#4D76B6', // 1
+            '#7CADD2', // 2
+            '#B0D9EA', // 3
+            '#E2F3F8', // 4
+            '#FEFFBC', // 5
+            '#FBDF8B', // 6
+            '#F6AD5A', // 7
+            '#EB6C3C', // 8
+            '#CE2D1F', // 9
+            '#9E0023'  // 10
+        ]);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', jsonURL);
+    xhr.send();
+    xhr.onload = function (d) {
+        let json = JSON.parse(d.currentTarget.responseText);
+        baseTemperature = json.baseTemperature;
+        createHeatMap(json, heatMap);
+    };
+
+
+    function convertNumberToMonth(int) {
+        switch (int) {
             case 1:
                 return "January";
                 break;
@@ -70,42 +102,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
                 break;
         }
     }
-
-    function calculateNewTemperature (base, variance) {
+    
+    function calculateNewTemperature(base, variance) {
         return base + variance;
     }
-
-    const padding = 40;
-    const heatMap = document.getElementById('heatMap');
-    const jsonURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
-    // const colorScheme = d3.scaleOrdinal(d3.schemeBlues[11]);
-    const color = d3.scaleOrdinal()
-    .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    .range([
-        '#343797', // 0
-        '#4D76B6', // 1
-        '#7CADD2', // 2
-        '#B0D9EA', // 3
-        '#E2F3F8', // 4
-        '#FEFFBC', // 5
-        '#FBDF8B', // 6
-        '#F6AD5A', // 7
-        '#EB6C3C', // 8
-        '#CE2D1F', // 9
-        '#9E0023'  // 10
-    ]);
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', jsonURL);
-    xhr.send();
-    xhr.onload = function(d){
-        let json = JSON.parse(d.currentTarget.responseText);
-        baseTemperature = json.baseTemperature;
-        console.log(json);
-        createHeatMap(json, heatMap);
-    };
-
-    function setDimensions(_parent){
+    
+    function setDimensions(_parent) {
         let parent = _parent;
         height = parent.clientHeight - margin.top - margin.bottom;
         width = parent.clientWidth - margin.left - margin.right;
@@ -114,90 +116,93 @@ window.addEventListener('DOMContentLoaded', ()=>{
         margin.bottom = (parent.clientHeight / 6) + 25;
         // margin.left = ;
     }
-
-    function createHeatMap(_data, _parent){
+    
+    function createHeatMap(_data, _parent) {
         setDimensions(_parent);
-        let data = _data;
-
+        let data = JSON.parse(JSON.stringify(_data));
+    
         console.log(data);
-
+    
         const minYear = d3.min(data.monthlyVariance, (d) => {
             return d.year;
         });
-
+    
         const maxYear = d3.max(data.monthlyVariance, (d) => {
             return d.year;
         });
 
-        const svg = d3.select(_parent)
-        .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom);
+        const yearsArray = [];
 
+        data.monthlyVariance.forEach(element => {
+            yearsArray.push(element.year);
+        });
+    
+        const svg = d3.select(_parent)
+            .append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom);
+    
         const xAxisGroup = svg.append('g')
             .attr('class', 'x-axis')
-            .attr('transform', `translate(0, ${height})`);
-
+            .attr('transform', `translate(${margin.left}, ${height + margin.top})`);
+    
         const yAxisGroup = svg.append('g')
             .attr('class', 'y-axis')
             .attr('transform', `translate(${margin.left}, ${margin.top})`)
-
-        
-        // const x = d3.scaleOrdinal()
-        //     .domain([minYear, maxYear])
-        //     .padding(0.2);
-
+    
+        const x = d3.scaleOrdinal()
+            // .domain([minYear, maxYear])
+            .domain(yearsArray)
+            .range([0, width]);
+    
         const y = d3.scaleBand()
             .domain(months)
             .rangeRound([0, height]);
-
+    
         const yAxisCall = d3.axisLeft(y);
-
+        const xAxisCall = d3.axisBottom(x);
+    
         yAxisGroup.call(yAxisCall);
-
-
-
+        xAxisGroup.call(xAxisCall);
+    
+    
+    
         // Heading Title
         svg.append('text')
-        .attr('id', 'title')
-        .attr('x', ()=>{
-            return width / 4;
-        })
-        .attr('text-anchor', 'center')
-        .attr('y', ()=>{
-            return padding;
-        })
-        .text('Monthly Global Land-Surface Temperature');
-
+            .attr('id', 'title')
+            .attr('x', () => {
+                return width / 4;
+            })
+            .attr('text-anchor', 'center')
+            .attr('y', () => {
+                return padding;
+            })
+            .text('Monthly Global Land-Surface Temperature');
+    
         // Heading Description
         svg.append('text')
-        .attr('id', 'description')
-        .attr('x', ()=>{
-            return width / 4;
-        })
-        .attr('y', ()=>{
-            return padding * 1.5;
-        })
-        .text('1753 - 2015: base temperature 8.66°C');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            .attr('id', 'description')
+            .attr('x', () => {
+                return width / 4;
+            })
+            .attr('y', () => {
+                return padding * 1.5;
+            })
+            .text('1753 - 2015: base temperature 8.66°C');
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         // need an x and y axis
         // need titles
         // need a legend
-
+    
         // const legend = svg.select('div')
         // .data(color)
         // .enter()
@@ -206,17 +211,17 @@ window.addEventListener('DOMContentLoaded', ()=>{
         //     console.log('d:', d);
         //     return d;
         // });
-
+    
         //left y axis
         //tick values
         //tick format
         //tick size?
         //tick padding?
-
+    
         // const yScale = d3.scaleOrdinal() //figure out how to space out y axis ticks
         // .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
         // .range([0, svgMeasurement.height]);
-
+    
         // const yAxis = d3.axisLeft()
         // .scale(yScale)
         // .tickValues(yScale.domain())
@@ -225,17 +230,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
         //     delta.setUTCMonth(d);
         //     return d3.utcFormat("%B")(delta);
         // });
-
+    
         // svg.append('g')
         // .call(yAxis)
         // .attr('transform', 'translate(' + padding + ', ' + padding + ')');
-        
-
-
-
-
+    
+    
+    
+    
+    
     }
-
-
 
 }); //end of doc ready
