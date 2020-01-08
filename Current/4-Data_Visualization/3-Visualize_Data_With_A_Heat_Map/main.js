6695fb96af99a877fb8716cfd903cc73a485d110
Mark <vindevccm@gmail.com>
2020-01-07 19:42:28 -0500

    progress on legend axis ticks

--- Current/4-Data_Visualization/3-Visualize_Data_With_A_Heat_Map/main.js
+++ Current/4-Data_Visualization/3-Visualize_Data_With_A_Heat_Map/main.js
@@ -28,24 +28,25 @@ window.addEventListener('DOMContentLoaded', () => {
     const testText = document.getElementById('test-text');
     const colorBox = document.getElementById('test-color-box');
     const colorArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const colorTextArray = [
+        '#343797', // 0
+        '#4D76B6', // 1
+        '#7CADD2', // 2
+        '#B0D9EA', // 3
+        '#E2F3F8', // 4
+        '#FEFFBC', // 5
+        '#FBDF8B', // 6
+        '#F6AD5A', // 7
+        '#EB6C3C', // 8
+        '#CE2D1F', // 9
+        '#9E0023' // 10
+    ];
     const padding = 40;
     const heatMap = document.getElementById('heatMap');
     const jsonURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
     const color = d3.scaleLinear()
         .domain(colorArray)
-        .range([
-            '#343797', // 0
-            '#4D76B6', // 1
-            '#7CADD2', // 2
-            '#B0D9EA', // 3
-            '#E2F3F8', // 4
-            '#FEFFBC', // 5
-            '#FBDF8B', // 6
-            '#F6AD5A', // 7
-            '#EB6C3C', // 8
-            '#CE2D1F', // 9
-            '#9E0023' // 10
-        ]);
+        .range(colorTextArray);
 
     let xhr = new XMLHttpRequest();
     xhr.open('GET', jsonURL);
@@ -106,20 +107,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
         console.log(data);
 
-        const minYear = d3.min(data.monthlyVariance, (d) => {
-            return d.year;
-        });
-
-        const maxYear = d3.max(data.monthlyVariance, (d) => {
-            return d.year;
-        });
-
-        const yearsArray = [];
-
-        data.monthlyVariance.forEach(element => {
-            yearsArray.push(element.year);
-        });
-
         const svg = d3.select(_parent)
             .append('svg')
             .attr('width', width + margin.left + margin.right)
@@ -151,7 +138,7 @@ window.addEventListener('DOMContentLoaded', () => {
         svg.append('text')
             .attr('id', 'title')
             .attr('x', () => {
-                return width / 4;
+                return width / 5;
             })
             .attr('text-anchor', 'center')
             .attr('y', () => {
@@ -163,33 +150,85 @@ window.addEventListener('DOMContentLoaded', () => {
         svg.append('text')
             .attr('id', 'description')
             .attr('x', () => {
-                return width / 4;
+                return width / 5;
             })
             .attr('y', () => {
                 return padding * 1.5;
             })
             .text('1753 - 2015: base temperature 8.66°C');
 
-        const legend = svg
-            .append('rect')
-            .attr('height', '100px')
-            .attr('width', '100px')
-            .style('fill', color(7));
-
 
         // draw out the map
         // draw out the legend
 
         // heat map
-        
-
 
 
 
 
         // legend
-        // get width of parent, add margin right and width of legend and set to x
-        // add margin top and set to y
+        const legendSize = {
+            height: 40,
+            width: 225
+        };
+
+        // const legend = svg.append('g')
+        //     .attr('y', 30)
+        //     .append('g')
+        //     .attr('height', legendSize.height)
+        //     .attr('width', legendSize.width)
+        //     .attr('x', function(){
+        //         return _parent.clientWidth - legendSize.width - 7.5;
+        //     })
+        //     .attr('x', 100)
+        //     .attr('y', 100)
+        // .attr('fill', function(){
+        //     return color(Math.floor(Math.random() * 10))
+        // });
+
+        const lowestTemp = d3.min(data.monthlyVariance, function (d) {
+            return data.baseTemperature + d.variance;
+        });
+
+        const highestTemp = d3.max(data.monthlyVariance, function (d) {
+            return data.baseTemperature + d.variance;
+        });
+
+        const xAxisGroupLegend = svg.append('g')
+            .attr('class', 'x-axis')
+            .attr('transform', `translate(${_parent.clientWidth - legendSize.width - 7.5}, ${30})`);
+
+        const xLegend = d3.scaleLinear()
+            .domain([lowestTemp, highestTemp])
+            .range([0, legendSize.width]);
+
+        const legendTicks = d3.scaleThreshold()
+            .domain(function (min, max, count) {
+                const result = [];
+                const step = (max - min) / count;
+                for (let i = 1; i < count; i++) {
+                    result.push((min + step * i));
+                }
+                return result;
+            })(lowestTemp, highestTemp, colorArray.length)
+            .range(colorTextArray);
+
+        xAxisGroupLegend.call(
+            d3.axisBottom(xLegend)
+            .ticks(legendTicks.domain(), '.1f')
+        );
+
+        xAxisGroupLegend.selectAll('g')
+            .data(colorArray)
+            .enter()
+            .append('rect')
+            .attr('x', function (d) {
+                // return (legendSize.width / colorArray.length) * d;
+                return 10 * d;
+            })
+            .attr('fill', function (d) {
+                return color(d);
+            });
 
 
 
