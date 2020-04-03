452880f930502a2b578438692eb44581aec1ad2c
Mark <vindevccm@gmail.com>
2020-04-03 16:30:40 -0400

    minor updates, started post thread route

--- Current/6-Information_Security_And_Quality_Assurance/5-Anonymous-Message-Board/routes/api.js
+++ Current/6-Information_Security_And_Quality_Assurance/5-Anonymous-Message-Board/routes/api.js
@@ -12,7 +12,26 @@ var expect = require('chai').expect;
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .post((req, res) => {
+      // text
+      // delete_password
+
+      /*
+        saved will be
+        _id
+        text
+        created_on
+        bumped_on
+        reported
+        deleted_password
+        replies
+      */
+
+      if(!req.body.hasOwnProperty('text')) res.status(400).send(`'text' property is required for this route.`);
+      if(!req.body.hasOwnProperty('delete_password')) res.status(400).send(`'delete_password' is required for this route.`);
+
+    });
     
   app.route('/api/replies/:board');
 
--- Current/6-Information_Security_And_Quality_Assurance/5-Anonymous-Message-Board/server.js
+++ Current/6-Information_Security_And_Quality_Assurance/5-Anonymous-Message-Board/server.js
@@ -1,15 +1,21 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+// process.env['NODE_ENV'] = 'test';
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+require('dotenv').config();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
 
-var app = express();
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+const helmet = require('helmet');
+
+const app = express();
+app.use(helmet());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+const serverPort = process.env.PORT || 3000;
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -20,17 +26,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sample front-end
 app.route('/b/:board/')
-  .get(function (req, res) {
+  .get((req, res) => {
     res.sendFile(process.cwd() + '/views/board.html');
   });
 app.route('/b/:board/:threadid')
-  .get(function (req, res) {
+  .get( (req, res) => {
     res.sendFile(process.cwd() + '/views/thread.html');
   });
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get( (req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
@@ -44,22 +50,22 @@ apiRoutes(app);
 
     
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+app.listen(serverPort, () => {
+  console.log("Listening on port " + serverPort);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout( () => {
       try {
         runner.run();
       } catch(e) {
-        var error = e;
+        const error = e;
           console.log('Tests are not valid:');
           console.log(error);
       }
