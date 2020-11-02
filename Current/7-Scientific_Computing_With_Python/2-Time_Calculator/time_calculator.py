e0ce70dbff93d18ee26f11d427e12146c0056c61
Mark <vindevccm@gmail.com>
2020-11-02 13:31:47 -0500

    added getweedayint function

--- Current/7-Scientific_Computing_With_Python/2-Time_Calculator/time_calculator.py
+++ Current/7-Scientific_Computing_With_Python/2-Time_Calculator/time_calculator.py
@@ -1,3 +1,12 @@
-def add_time(start, duration):
+def add_time(start, duration, dayOfWeek=None):
+    # example input = "11:06 PM", "2:02"
+    # getWeekDayInt('TuEsDay') # returns 2
+
+    
 
     return 'foo'
+
+def getWeekDayInt(day):
+    day = day.lower()
+    weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday')
+    return weekdays.index(day)
