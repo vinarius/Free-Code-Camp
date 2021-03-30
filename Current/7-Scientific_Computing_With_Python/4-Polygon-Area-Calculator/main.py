bf444e0473ebb06c8d4bc3878843f64f6105dc01
vinarius <vinarius@MARKK-P53S.localdomain>
2021-03-29 21:54:18 -0400

    completed polygon area calculator project

--- Current/7-Scientific_Computing_With_Python/4-Polygon-Area-Calculator/main.py
+++ Current/7-Scientific_Computing_With_Python/4-Polygon-Area-Calculator/main.py
@@ -2,19 +2,10 @@
 import shape_calculator
 from unittest import main
 
+rect = shape_calculator.Rectangle(4, 8)
+rect2 = shape_calculator.Rectangle(3, 6)
 
-rect = shape_calculator.Rectangle(5, 10)
-print(rect.get_area())
-rect.set_width(3)
-print(rect.get_perimeter())
-print(rect)
-
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-
+print(rect.get_amount_inside(rect2))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
